74750a588f2c4bef1f14e532402f4cb6238bbdc8
dinhchuyen <nguyendinhchuyen96@gmail.com>
2022-03-16 10:47:18 +0700

    add file

---
+++ resources/views/site/exam.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.site')
+@section('title','Home')
+@section('main')
+    <nav class="nav justify-content-center">
+      <a class="nav-link active" href="#">Active link</a>
+      <a class="nav-link" href="#">Link</a>
+      <a class="nav-link disabled" href="#">Disabled link</a>
+    </nav>
+@stop()
